6be4bbf5fc4579c796a1904b8cdf8f6ba5040769
Derek Reynolds <derekr@me.com>
2022-10-01 17:35:02 -0700

    Try hidden hcard

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -37,59 +37,61 @@ const ExperienceItem = ({ title, href, startYear, endYear, children }) => (
 )
 
 export default function Index() {
-  const { globalSettings, workExperience } = useLoaderData<typeof loader>()
+  const { author, globalSettings, workExperience } = useLoaderData<typeof loader>()
   const [currentExperience, previousExperience] = fork(workExperience, (e: any) => e.isCurrent)
   return (
     <>
-      <>
-        <div className="max-w-2xl mb-8 mast-grid [&>p:first-of-type]:mb-3">
-          <h1 className="text-lg">
-            <a href="/">drk.wtf</a>
-          </h1>
-          <SanityContent value={globalSettings[0]?.aboutMe} />
+      <a className="sr-only h-card" href="https://drk.wtf">
+        <img src={author.imageUrl} alt="" />
+        Derek Reynolds
+      </a>
+      <div className="max-w-2xl mb-8 mast-grid [&>p:first-of-type]:mb-3">
+        <h1 className="text-lg">
+          <a href="/">drk.wtf</a>
+        </h1>
+        <SanityContent value={globalSettings[0]?.aboutMe} />
+      </div>
+      <div className="grid mb-5 experience-grid md:grid-cols-3">
+        <div className="space-y-4 experience md:col-start-2 [&>article:last-of-type]:mb-3">
+          <h2 className="text-gray-600 text-md">Currently</h2>
+          {currentExperience.map((item) => {
+            return (
+              <ExperienceItem
+                key={item.title}
+                title={item.title}
+                href={item.url ?? ''}
+                startYear={new Date(item.startDate).getFullYear()}
+                endYear={item.endDate ? new Date(item.endDate).getFullYear() : 'Present'}
+              >
+                <SanityContent value={item.body} />
+              </ExperienceItem>
+            )
+          })}
+          <br />
+          <h2 className="text-gray-600 text-md">Previously</h2>
+          {previousExperience.map((item) => {
+            return (
+              <ExperienceItem
+                key={item.title}
+                title={item.title}
+                href={item.url ?? ''}
+                startYear={new Date(item.startDate).getFullYear()}
+                endYear={item.endDate ? new Date(item.endDate).getFullYear() : 'Present'}
+              >
+                <SanityContent value={item.body} />
+              </ExperienceItem>
+            )
+          })}
+          <a
+            href="https://www.linkedin.com/in/derek-reynolds-11a806132/"
+            target="_blank"
+            className="block text-blue-600 underline hover:text-blue-800 visited:text-purple-600"
+          >
+            Linkedin {'>>'}
+          </a>
         </div>
-        <div className="grid mb-5 experience-grid md:grid-cols-3">
-          <div className="space-y-4 experience md:col-start-2 [&>article:last-of-type]:mb-3">
-            <h2 className="text-gray-600 text-md">Currently</h2>
-            {currentExperience.map((item) => {
-              return (
-                <ExperienceItem
-                  key={item.title}
-                  title={item.title}
-                  href={item.url ?? ''}
-                  startYear={new Date(item.startDate).getFullYear()}
-                  endYear={item.endDate ? new Date(item.endDate).getFullYear() : 'Present'}
-                >
-                  <SanityContent value={item.body} />
-                </ExperienceItem>
-              )
-            })}
-            <br />
-            <h2 className="text-gray-600 text-md">Previously</h2>
-            {previousExperience.map((item) => {
-              return (
-                <ExperienceItem
-                  key={item.title}
-                  title={item.title}
-                  href={item.url ?? ''}
-                  startYear={new Date(item.startDate).getFullYear()}
-                  endYear={item.endDate ? new Date(item.endDate).getFullYear() : 'Present'}
-                >
-                  <SanityContent value={item.body} />
-                </ExperienceItem>
-              )
-            })}
-            <a
-              href="https://www.linkedin.com/in/derek-reynolds-11a806132/"
-              target="_blank"
-              className="block text-blue-600 underline hover:text-blue-800 visited:text-purple-600"
-            >
-              Linkedin {'>>'}
-            </a>
-          </div>
-        </div>
-        <footer>© Derek Reynolds {new Date().getFullYear()}</footer>
-      </>
+      </div>
+      <footer>© Derek Reynolds {new Date().getFullYear()}</footer>
     </>
   )
 }
--- app/sanity/queries/home-page.ts
+++ app/sanity/queries/home-page.ts
@@ -3,6 +3,7 @@ import groq from 'groq'
 export default groq`
 {
   "globalSettings": *[_type == "globalSettings"]{ aboutMe },
+  "author": *[_type == "author"][0]{ "imageUrl": image.asset->url },
   "workExperience": *[_type == "cv.experience"]{ 
     title,
     url,
