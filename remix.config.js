a1e8b00057dcd05ae97e34fbe1170659d7bd24a3
Derek Reynolds <derekr@users.noreply.github.com>
2022-10-06 10:11:13 -0700

    Correct config

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,14 @@
 const React = require('react')
 
 /**
- * @type {import('@remix-run/dev/config').AppConfig}
+ * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
   serverBuildDirectory: 'api/_build',
-  serverDependencies: ['shiki'],
+  serverDependenciesToBundle: ['shiki'],
   ignoredRouteFiles: ['.*'],
   mdx: async (filename) => {
     const [
