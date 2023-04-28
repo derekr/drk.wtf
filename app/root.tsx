8563e43003fb8ea083f90eee489b87ea2d818789
Derek Reynolds <derekr@users.noreply.github.com>
2023-04-28 06:37:03 -0700

    Fix root imports

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
-import { json } from '@remix-run/node'
-import type { MetaFunction } from '@remix-run/node'
+import { json } from '@remix-run/server-runtime'
+import type { MetaFunction } from '@remix-run/server-runtime'
 import { useLoaderData, useLocation } from '@remix-run/react'
 
 import { projectDetails } from '~/sanity/config'
