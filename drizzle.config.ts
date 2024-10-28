07ef23544c63970f6f71c3fdf309694144862891
Derek Reynolds <derekr@users.noreply.github.com>
2024-10-27 21:42:47 -0700

    Biome fixes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-	schema: './app/db/schema.server.ts',
-	out: './app/db/migrations',
-	dialect: 'sqlite',
-	driver: 'd1-http',
-	dbCredentials: {
-		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-		token: process.env.CLOUDFLARE_D1_TOKEN!,
-	},
+  schema: "./app/db/schema.server.ts",
+  out: "./app/db/migrations",
+  dialect: "sqlite",
+  driver: "d1-http",
+  dbCredentials: {
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    token: process.env.CLOUDFLARE_D1_TOKEN!,
+  },
 });
