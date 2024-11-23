034cc4086db1c0cb7f8fb65a8164625650d1fdf9
Derek Reynolds <derekr@users.noreply.github.com>
2024-11-22 21:39:05 -0800

    Prop for react-router upgrade

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,41 +4,41 @@
  * For more information, see https://remix.run/file-conventions/entry.server
  */
 
-import type { AppLoadContext, EntryContext } from '@remix-run/cloudflare';
-import { RemixServer } from '@remix-run/react';
-import { isbot } from 'isbot';
-import { renderToReadableStream } from 'react-dom/server';
+import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
+import { RemixServer } from "@remix-run/react";
+import { isbot } from "isbot";
+import { renderToReadableStream } from "react-dom/server";
 
 export default async function handleRequest(
-	request: Request,
-	responseStatusCode: number,
-	responseHeaders: Headers,
-	remixContext: EntryContext,
-	// This is ignored so we can keep it in the template for visibility.  Feel
-	// free to delete this parameter in your app if you're not using it!
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	loadContext: AppLoadContext,
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+  // This is ignored so we can keep it in the template for visibility.  Feel
+  // free to delete this parameter in your app if you're not using it!
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  loadContext: AppLoadContext
 ) {
-	let statusCode = responseStatusCode;
-	const body = await renderToReadableStream(
-		<RemixServer context={remixContext} url={request.url} />,
-		{
-			signal: request.signal,
-			onError(error: unknown) {
-				// Log streaming rendering errors from inside the shell
-				console.error(error);
-				statusCode = 500;
-			},
-		},
-	);
+  let statusCode = responseStatusCode;
+  const body = await renderToReadableStream(
+    <RemixServer context={remixContext} url={request.url} />,
+    {
+      signal: request.signal,
+      onError(error: unknown) {
+        // Log streaming rendering errors from inside the shell
+        console.error(error);
+        statusCode = 500;
+      },
+    }
+  );
 
-	if (isbot(request.headers.get('user-agent') || '')) {
-		await body.allReady;
-	}
+  if (isbot(request.headers.get("user-agent") || "")) {
+    await body.allReady;
+  }
 
-	responseHeaders.set('Content-Type', 'text/html');
-	return new Response(body, {
-		headers: responseHeaders,
-		status: statusCode,
-	});
+  responseHeaders.set("Content-Type", "text/html");
+  return new Response(body, {
+    headers: responseHeaders,
+    status: statusCode,
+  });
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,34 @@
 import {
-	vitePlugin as remix,
-	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from '@remix-run/dev';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { visualizer } from 'rollup-plugin-visualizer';
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
 
-declare module '@remix-run/cloudflare' {
-	interface Future {
-		v3_singleFetch: true;
-	}
+declare module "@remix-run/cloudflare" {
+  interface Future {
+    v3_singleFetch: true;
+  }
 }
 
 export default defineConfig({
-	plugins: [
-		remixCloudflareDevProxy(),
-		remix({
-			future: {
-				v3_singleFetch: true,
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-				v3_throwAbortReason: true,
-				v3_lazyRouteDiscovery: true,
-			},
-		}),
-		tsconfigPaths(),
-		visualizer({
-			emitFile: true,
-			filename: 'stats.html',
-		}),
-	],
+  plugins: [
+    remixCloudflareDevProxy(),
+    remix({
+      future: {
+        v3_singleFetch: true,
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_routeConfig: true,
+      },
+    }),
+    tsconfigPaths(),
+    visualizer({
+      emitFile: true,
+      filename: "stats.html",
+    }),
+  ],
 });
